ceb165fc641a3211e9e7a2007a29c718caf865a3
vhetet <vhetet@solstice.com>
2019-07-20 15:33:43 -0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import CreateRecordView from '../src/components/create-record/CreateRecordView'
+import CreateRecordView from './components/create-record/CreateRecordView'
+import HomeView from './components/home/HomeView'
 
 function App() {
   return (
     <div className="App">
-      <CreateRecordView />
+      <Router>
+        <ul>
+          <li>
+            <Link to="">Home</Link>
+          </li>
+          <li>
+            <Link to="/create-record">Create Record</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={HomeView} />
+        <Route path="/create-record" component={CreateRecordView} />
+      </Router>
     </div>
   );
 }
