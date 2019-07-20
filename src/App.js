1c6d492d487a903599bac34f05dc3c1a77fee098
vhetet <vhetet@solstice.com>
2019-07-20 17:54:29 -0500

    add blockchain auth field and refactor form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import CreateRecordView from './components/create-record/CreateRecordView'
-import HomeView from './components/home/HomeView'
+// import HomeView from './components/home/HomeView'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <ul>
+        {/* <ul>
           <li>
             <Link to="">Home</Link>
           </li>
           <li>
             <Link to="/create-record">Create Record</Link>
           </li>
-        </ul>
-        <Route exact path="/" component={HomeView} />
-        <Route path="/create-record" component={CreateRecordView} />
+        </ul> */}
+        {/* <Route exact path="/" component={HomeView} /> */}
+        {/* <Route path="/create-record" component={CreateRecordView} /> */}
+        <Route path="" component={CreateRecordView} />
       </Router>
     </div>
   );
